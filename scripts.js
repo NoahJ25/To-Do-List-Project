ec3619a376791bc1b63da0a80816c0d9123a94ab
NoahJ25 <zerocaesar20@gmail.com>
2025-05-11 17:07:01 -0700

    restarting project

---
+++ scripts.js
@@ -0,0 +1,16 @@
+console.log("Script File Test");
+
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert("You must write something!");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+    }
+}
+
